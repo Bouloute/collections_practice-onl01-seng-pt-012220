21eab1261ee1650ec502ef2bdf94f6b38885fec2
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-27 19:38:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  array.sort{|a,b|
+    a<=>b
+  }
+end
 
+def sort_array_desc(array)
+  array.sort{|a,b|
+    b<=>a
+  }
+end
+
+def sort_array_char_count(array)
+  array.sort{|a,b|
+    a.size<=>b.size
+  }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def swap_elements_from_to(array, first, second)
+  array[first - 1], array[second - 1] = array[second - 1], array[first - 1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse #probably not what has been asked?
+end
+
+def kesha_maker(array)
+  array.each{|word|
+    word[2] = "$"
+  }
+  array
+end
+
+def find_a(array)
+  array.select{|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+  #array.reduce(:+)
+  array.reduce{|sum, number| sum + number}
+  #array.inject(:+)
+end
